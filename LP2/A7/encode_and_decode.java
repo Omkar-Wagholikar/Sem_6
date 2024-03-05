89a967ad367ddd90227a85127e8a0dda0f56be79
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2024-03-05 23:54:11 +0530

    encode and decode working

--- LP2/A7/encode_and_decode.java
+++ LP2/A7/encode_and_decode.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Codechef {
+public class t {
     static String key;
     static class Block{
         char value;
@@ -40,57 +40,81 @@ public class Codechef {
             }
         }
 
-        for(Block bloc: matrix){
-            System.out.println(bloc.value + ": "+ bloc.val.toString());
-        }
-
         Arrays.sort(matrix, (a, b) -> a.index - b.index);
-
-        System.out.println();
-        for(Block bloc: matrix){
-            System.out.println(bloc.value + ": "+ bloc.val.toString());
-        }
-
+		
         String cypherText = "";
         for(Block bloc: matrix){
             for(char i : bloc.val) cypherText += i;
         }
         return cypherText;
     }
-    public static void main(String[] args) {
-        key = "EXAMPLE";
-        // String plainText = "OMKARISHERE";
-        //  E X A M P L E
-        //  O M K A R I S 
-        //  H E R E _ _ _
-        
-        //  A E E L M P X
-        //  K O S I A R M
-        //  R H _ _ E _ E
 
-        // K R O H S _ I _ A E R _ M E
-        String cypherText = "KROHS_I_AER_ME";
-        System.out.println(cypherText);
-
+    public static String decode(String cypherText){
         Block [] matrix = new Block[key.length()];        
         for(int i=0; i<key.length(); i++) {
-            matrix[i] = new Block(key.charAt(i));
+            matrix[i] = new Block('_');
+            matrix[i].value = key.charAt(i);
             matrix[i].index = i;
         }
         
         Arrays.sort(matrix, (a, b) -> a.value - b.value);
-
-        int height = cypherText.length() / key.length();
-
-        for(int i=0; i<cypherText.length(); i+=height){
+        
+        int cypherTextLength = cypherText.length();
+        int height = cypherTextLength / key.length();
+		
+        for(int i=0; i<key.length(); i++){
             for(int j=0; j<height; j++){
-                matrix[i].val.add(cypherText.charAt(i+j));
+                matrix[i].val.add(cypherText.charAt(height*i+j));
             }
         }
 
+        Arrays.sort(matrix, (a,b)->a.index-b.index);
 
-        for(Block bloc: matrix){
-            System.out.println(bloc.value + ": "+ bloc.val.toString());
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i<matrix[0].val.size(); i++){
+            for(int r =0; r<key.length(); r++){
+                char temp = matrix[r].val.get(i);
+                if(temp != '_') res.append(temp);
+            }
         }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        // key = "EXAMPLE";
+
+        // String plainText = "OMKARISHERE";
+        // String c = encode(plainText);
+        // System.out.println("Cypher text: "+ c);
+		// String p = decode(c);
+        // System.out.println("Plain text: " + p);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter key: ");
+		key = sc.nextLine();
+		int v= 4;
+		while(v-- > 0){
+			System.out.print("Enter Plain text: ");
+			String p = sc.nextLine();
+			String c = encode(p);
+			System.out.println("Cypher text: "+c);
+			p = decode(c);
+			System.out.println("Plain text:  "+p);
+
+		}
+        // //  E X A M P L E
+        // //  O M K A R I S 
+        // //  H E R E _ _ _
+        
+        // //  A E E L M P X
+        // //  K O S I A R M
+        // //  R H _ _ E _ E
+
+        // //  0 2 4 6 8 0 2
+        // //  1 3 5 7 9 1 3
+
+        // // K R O H S _ I _ A E R _ M E
+        // // 0 1 2 3 4 5 6 7 8 9 0 1 2 3
     }
 }
